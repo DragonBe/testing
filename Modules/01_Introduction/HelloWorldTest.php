63239e59f03198b5c7dd97b754f5bd9f73da24ed
michelangelo <dragonbe+github@gmail.com>
2014-03-07 14:07:01 +0100

    Improving unit test for HelloWorld example

--- Modules/01_Introduction/HelloWorldTest.php
+++ Modules/01_Introduction/HelloWorldTest.php
@@ -5,7 +5,11 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
     public function testSayReturnsHelloWorld()
     {
-        $hw = new HelloWorld();
-	$this->assertSame("Hello World!", $hw->say());
+        $helloWorld = new HelloWorld();
+        $result = $helloWorld->say();
+        $expected = 'Hello World!';
+
+	$this->assertSame($expected, $result,
+        'We didn\'t got a hello world message');
     }
 }
